fec9e50194557c8820ee73e5333b772e0bcc8626
Knhye <knahye311@gmail.com>
2026-03-20 23:48:09 +0900

    feat: 카테고리, 난이도 목록 수정

--- server/src/courses/entities/enums/category.enum.ts
+++ server/src/courses/entities/enums/category.enum.ts
@@ -1,1 +1,9 @@
-export enum Category {}
+export enum Category {
+  KOREAN = 'KOREAN', // 한식
+  JAPANESE = 'JAPANESE', // 일식
+  CHINESE = 'CHINESE', // 중식
+  WESTERN = 'WESTERN', // 양식
+  BAKING = 'BAKING', // 베이킹
+  SIDE_DISH = 'SIDE_DISH', // 반찬
+  ONE_DISH = 'ONE_DISH', // 일품요리
+}
--- server/src/courses/entities/enums/difficulty.enum.ts
+++ server/src/courses/entities/enums/difficulty.enum.ts
@@ -1,1 +1,5 @@
-export enum Difficulty {}
+export enum Difficulty {
+  HIGH = 'HIGH', // 상
+  MEDIUM = 'MEDIUM', // 중
+  LOW = 'LOW', // 하
+}
