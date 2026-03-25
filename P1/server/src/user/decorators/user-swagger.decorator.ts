f93f676e74c9edcc3785d2b86ae28330ea5a7068
Knhye <knahye311@gmail.com>
2026-03-25 23:48:51 +0900

    feat: 데코레이터 설정

--- P1/server/src/user/decorators/user-swagger.decorator.ts
+++ P1/server/src/user/decorators/user-swagger.decorator.ts
@@ -8,3 +8,11 @@ export const GetMe = () =>
       description: '내 정보 상세 보기',
     }),
   );
+
+export const UpdateMe = () =>
+  applyDecorators(
+    ApiOperation({
+      summary: '내 정보 수정',
+      description: '이메일, 닉네임 수정',
+    }),
+  );
--- P1/server/src/user/user.controller.ts
+++ P1/server/src/user/user.controller.ts
@@ -1,11 +1,20 @@
 import { JwtAccessGuard } from '@auth/guards/jwt-access.guard';
 import { GetUser } from '@common/decorators/get-user.decorator';
-import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { ApiResponseDto } from '@common/dto/api-response.dto';
 import { UserDetailResponse } from './dto/user-detail-response.dto';
-import { GetMe } from './decorators/user-swagger.decorator';
+import { GetMe, UpdateMe } from './decorators/user-swagger.decorator';
+import { UpdateUserRequestDto } from './dto/update-user-request.dto';
+import { UpdateUserResponseDto } from './dto/update-user-response.dto';
 
 @Controller('user')
 @UseGuards(JwtAccessGuard)
@@ -25,5 +34,16 @@ export class UserController {
       '내 정보 조회에 성공하였습니다.',
       HttpStatus.OK,
     );
+  }
+
+  @Patch('me')
+  @UpdateMe()
+  async updateMe(
+    @GetUser('id') userId: string,
+    @Body() dto: UpdateUserRequestDto,
+  ): Promise<ApiResponseDto<UpdateUserResponseDto>> {
+    const data = await this.userService.updateMe(userId, dto);
+
+    return ApiResponseDto.success(data, '수정 성공', HttpStatus.OK);
   }
 }
